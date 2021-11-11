14f64285efff4f63333678febe065b400ed9fbc7
chanda <11214chanda@gmail.com>
2021-11-11 22:18:24 +0700

    get input from view

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -8,23 +8,48 @@ class DashboardsController < ApplicationController
 		@dep = params[:id]
 	end
 
+	def time_plan
+		@assign_time = params[:emp]
+		@uID_list = params[:select_user]
+		#puts @uID_list.class
+		if @uID_list
+		@uID_list.each do |uID|
+			user = User.find(uID.to_i)
+			@assign = user.plans.new(@assign_time)
+			#@assign_time[:user_id] = uID.to_i
+			#puts @assign_time
+			#puts ""
+			@assign = Plan.new(@assign_time)
+		end
+		end
+	end
+
 	def edit
 		@dep = params[:id]
 		@users = User.where(:department1 => @dep)
 		@employee = @users.select{|user| user.position=="employee"}
-	end
 
-	def time_plan
-		@em = User.find(2)
-		params.require(:plans).permit(:date, :start_time, :stop_time, :ot, @em)
-	end
-
-	def assign_shift
-		@assign = Plan.new(time_plan)
-		if @assign.save
-			#if creation is successful, show up 'successful' message
-			flash[:notice] = "successful"
-			redirect_to @users
+		@assign_time = params[:emp]
+		@uID_list = params[:select_user]
+		#puts @uID_list.class
+		if @uID_list
+		@uID_list.each do |uID|
+			user = User.find(uID.to_i)
+			#@assign = user.Plan.new(@assign_time)
+			@assign_time[:user_id] = uID.to_i
+			puts @assign_time
+			puts ""
+			#@assign = Plan.new(@assign_time)
 		end
+		end
+
+		#if @assign.save
+			#if creation is successful, show up 'successful' message
+		#	flash[:notice] = "successful"
+		#	redirect_to edit_dashboard_path(@dep)
+		#else
+			#render 'edit'
+		#end
+		
 	end
 end
