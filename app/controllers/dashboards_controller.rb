aca5ef02c51555590ae8eb2be2481fd79997dd7d
chanda <11214chanda@gmail.com>
2021-11-12 13:50:08 +0700

    update database

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -7,18 +7,27 @@ class DashboardsController < ApplicationController
 	def show
 		@dep = params[:id]
 	end
-
+	require 'date'
 	def time_plan
-		params.require(:emp).permit(:date, :time_in, :time_out, :OT)
+		# get input from view
+		data = params.require(:emp).permit(:date, :time_in, :time_out, :OT)
+		# edit date(time in/out) => date
+		[1,2,3].each do |n|
+			data["time_in(#{n}i)"] = data["date(#{n}i)"]
+			data["time_out(#{n}i)"] = data["date(#{n}i)"]
+		end
+		return data
 	end
 
 	def edit
 		@dep = params[:id]
 		@users = User.where(:department1 => @dep)
 		@employee = @users.select{|user| user.position=="employee"}
+		# list of user id (tick checkbox)
 		@uID_list = params[:select_user]
 
 		if @uID_list
+			# insert each data to Plan database
 			@uID_list.each do |uID|
 				@assign = Plan.create(time_plan)
 				user = User.find(uID.to_i)
